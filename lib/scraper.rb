491b27b28a5a9e0d2c461a72971c1c74e3196aaa
Lisa Feng <lisafeng10@gmail.com>
2020-07-03 18:02:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,5 +5,5 @@ html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
 
-puts doc.css(".headline-26OIBN").text
+puts doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
 
